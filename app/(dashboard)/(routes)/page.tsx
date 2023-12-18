4441299fb677012ffd4840d92b23390cf1e7bdd1
John Doe1~ <johndoe@example.com>
2023-12-18 21:39:35 +0300

    Add `<UserButton/>` from Clerk to test an integration with that service

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function HomePage() {
   return (
     <div>
-      <Button variant={'ghost'}>Click</Button>
-      <p className='text-2xl text-red-500 lg:text-4xl'>Hello world</p>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
