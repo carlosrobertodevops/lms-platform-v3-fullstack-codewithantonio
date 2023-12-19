0a7af464ea0af89b1d375dddba2b72a670201b8a
John Doe1~ <johndoe@example.com>
2023-12-19 21:14:23 +0300

    Cоздал константу `GUEST_ ROUTES` и присвоил её значение переменной `routes`

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,22 @@
+import { Compass, Layout } from 'lucide-react';
+
 `use client`;
 
+const GUEST_ROUTES = [
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+  {
+    icon: Compass,
+    label: 'Browse',
+    href: '/search',
+  },
+];
+
 const SidebarRoutes = () => {
+  const routes = GUEST_ROUTES;
   return <div className='flex flex-col'>This is a sidebar routes</div>;
 };
 
