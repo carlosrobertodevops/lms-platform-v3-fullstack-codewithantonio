12ad46c819dcdc3a05a2fa4540612de725e68ab7
John Doe1~ <johndoe@example.com>
2023-12-19 20:54:06 +0300

    Добавил `<Logo/>` c помощью сниппета, импортировал его в `<Sidebar/>` и обернул его в `div`

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,6 +1,12 @@
+import Logo from './logo';
+
 const Sidebar = () => {
   return (
-    <div className='flex flex-col overflow-y-auto border-r bg-white shadow-sm '></div>
+    <div className='flex flex-col overflow-y-auto border-r bg-white shadow-sm '>
+      <div className='p-6'>
+        <Logo />
+      </div>
+    </div>
   );
 };
 
