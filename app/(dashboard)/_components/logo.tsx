afde65f9a9662ab1cf63c3454cfc67633ca9d391
John Doe1~ <johndoe@example.com>
2023-12-19 21:02:41 +0300

    Добавил `svg` в `<Logo/>` c помощью компонента `<Image/>` из библиотеки `next/image`

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,9 @@
+import Image from 'next/image';
+
 const Logo = () => {
-  return <div>This is a logo</div>;
+  return (
+    <Image alt='Logoipsum' src={'/images/logo.svg'} width={130} height={130} />
+  );
 };
 
 export default Logo;
