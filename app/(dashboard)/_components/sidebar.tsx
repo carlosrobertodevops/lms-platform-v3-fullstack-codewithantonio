a173b6c67303c46bf8217402943f2db9ccd7ac21
John Doe1~ <johndoe@example.com>
2023-12-19 20:47:10 +0300

    Создал `<Sidebar/>` c помощью сниппета и импортировал его в `<DashboardLayout/>`

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,5 @@
+const Sidebar = () => {
+  return <div>This is a sidebar</div>;
+};
+
+export default Sidebar;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,11 @@
+import Sidebar from './_components/sidebar';
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full'>
-      <div className='fixed z-50 hidden h-full w-56 md:flex'></div>
+      <div className='fixed z-50 hidden h-full w-56 md:flex'>
+        <Sidebar />
+      </div>
       {children}
     </div>
   );
