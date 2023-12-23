9006a9f153339b571412f71854c4516e672fb696
John Doe1~ <johndoe@example.com>
2023-12-23 14:18:06 +0300

    Добавил стили и иконку для триггера `<Sheet/>` компонента

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,9 +1,12 @@
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Menu } from 'lucide-react';
 
 const MobileSidebar = () => {
   return (
     <Sheet>
-      <SheetTrigger>Button</SheetTrigger>
+      <SheetTrigger className='mr-4 transition hover:opacity-75 md:hidden'>
+        <Menu />
+      </SheetTrigger>
       <SheetContent>Content</SheetContent>
     </Sheet>
   );
