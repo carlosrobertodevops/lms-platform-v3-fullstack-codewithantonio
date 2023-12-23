1b3f99e652432af87371eeec43485b16fd4d8328
John Doe1~ <johndoe@example.com>
2023-12-23 14:55:10 +0300

    Добавил динамический рендеринг компонента <Button/> из библиотеки `Shadcn` в зависимости от текущей страницы и стилизовал его

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -2,15 +2,25 @@
 
 import { UserButton } from '@clerk/nextjs';
 import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith('/teacher');
-  const isCoursePage = pathname?.includes('/chapter');
+  const isPlayerPage = pathname?.includes('/chapters');
 
   return (
     <div className='ml-auto flex items-center gap-x-2'>
+      {isTeacherPage || isPlayerPage ? (
+        <Button size={'sm'} variant={'ghost'}>
+          Exit
+        </Button>
+      ) : (
+        <Button size={'sm'} variant={'ghost'}>
+          Teacher Mode
+        </Button>
+      )}
       <UserButton afterSignOutUrl='/' />
     </div>
   );
