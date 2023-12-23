915f0164638099ae46d1549bdeb82256cdf322b1
John Doe1~ <johndoe@example.com>
2023-12-23 14:30:09 +0300

    Изменил значения по умолчанию в `<SheetContent/>` и добавил в него `<Sidebar/>`

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,5 +1,6 @@
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Menu } from 'lucide-react';
+import Sidebar from './sidebar';
 
 const MobileSidebar = () => {
   return (
@@ -7,7 +8,9 @@ const MobileSidebar = () => {
       <SheetTrigger className='mr-4 transition hover:opacity-75 md:hidden'>
         <Menu />
       </SheetTrigger>
-      <SheetContent>Content</SheetContent>
+      <SheetContent side={'left'} className='border-none p-0'>
+        <Sidebar />
+      </SheetContent>
     </Sheet>
   );
 };
