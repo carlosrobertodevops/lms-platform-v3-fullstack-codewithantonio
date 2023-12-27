047e03471268f0649f8f12b71cf1a0a048fd7975
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-27 21:02:53 +0300

    Добавил функцию-обработчик `handleSubmit`
    - Сделал все инпути обязательными
    
    - Добавил нативную браузерную валидацию на минимальное количество символов для поля `password`
    
    - Добавил проп `onSubmit` для тега `form` и передал в него функцию-обработчик `handleSubmit`
    
    - Отменил действие по умолчанию для функции-обрабочика
    
    - Вывел в консоль текущее состояние полей после отправки формы

--- app/(sandbox)/(routes)/form-without-react-hook-form/page.tsx
+++ app/(sandbox)/(routes)/form-without-react-hook-form/page.tsx
@@ -1,19 +1,25 @@
 'use client';
 
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 const FormWithoutReactHookForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log({ email, password, confirmPassword });
+  };
+
   return (
-    <form className='flex flex-col gap-y-2 '>
+    <form className='flex flex-col gap-y-2' onSubmit={handleSubmit}>
       <input
         type='email'
         className='rounded px-4 py-2'
         placeholder='email'
         value={email}
+        required
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
@@ -21,6 +27,8 @@ const FormWithoutReactHookForm = () => {
         className='rounded px-4 py-2'
         placeholder='password'
         value={password}
+        required
+        minLength={6}
         onChange={(e) => setPassword(e.target.value)}
       />
       <input
@@ -28,6 +36,7 @@ const FormWithoutReactHookForm = () => {
         className='rounded px-4 py-2'
         placeholder='confirm password'
         value={confirmPassword}
+        required
         onChange={(event) => setConfirmPassword(event.target.value)}
       />
       <button
