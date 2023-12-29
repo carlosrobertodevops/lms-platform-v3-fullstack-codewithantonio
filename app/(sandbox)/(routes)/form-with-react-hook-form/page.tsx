0bc97da3929434e9e0f268a0995582f5196163e2
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-29 10:01:27 +0300

    Очистил разметку и получил значения из хука `useForm`
    - Удалил стейты инпутов и ошибок.
    
    - Импортировал и вызвал хук `useForm`, деструктурировал полученные значения.
    
    - Удалил функцию-обработчик `handleSubmit`, так как мы получили функцию с тем же названием из вызова хука `useForm`
    
    - Удалил отображение массива `errors` в разметке
    
    - У инпутов удалил пропы `onChange` и `value`
    
    - Удалил проп `disabled` у кнопки
    
    - Удалил проп `onSubmit` у формы, который вызывает удаленную функцию `handleSubmit`

--- app/(sandbox)/(routes)/form-with-react-hook-form/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form/page.tsx
@@ -1,80 +1,38 @@
 import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const FormWithReactHookForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [errors, setErrors] = useState<string[]>([]);
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setIsSubmitting(true);
-
-    if (password !== confirmPassword) {
-      setErrors(['Пароли не совпадают']);
-      setIsSubmitting(false);
-
-      return;
-    }
-
-    const result = await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('success');
-      }, 2000);
-    });
-
-    console.log(result);
-
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-    setErrors([]);
-
-    setIsSubmitting(false);
-  };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { isSubmitting, isValid, errors },
+  } = useForm();
 
   return (
-    <form className='flex flex-col gap-y-2' onSubmit={handleSubmit}>
-      {errors.length > 0 && (
-        <ul>
-          {errors.map((error) => (
-            <li
-              key={error}
-              className='rounded bg-red-100 px-4 py-2 text-center text-red-500'>
-              {error}
-            </li>
-          ))}
-        </ul>
-      )}
+    <form className='flex flex-col gap-y-2'>
       <input
         type='email'
         className='rounded px-4 py-2'
         placeholder='email'
-        value={email}
         required
-        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type='password'
         className='rounded px-4 py-2'
         placeholder='password'
-        value={password}
         required
         minLength={6}
-        onChange={(e) => setPassword(e.target.value)}
       />
       <input
         type='password'
         className='rounded px-4 py-2'
         placeholder='confirm password'
-        value={confirmPassword}
         required
-        onChange={(event) => setConfirmPassword(event.target.value)}
       />
       <button
         type='submit'
-        disabled={isSubmitting}
         className='rounded bg-green-500 py-2 text-lg font-medium text-white disabled:bg-gray-500'>
         Отправить
       </button>
