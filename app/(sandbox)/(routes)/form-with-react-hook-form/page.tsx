a99e4223178be6451bb38091dd9c415a67914098
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-29 10:53:04 +0300

    Отображение массива ошибок
    - Добавил режим отображения ошибок
    
    - Добавил паттерн для валидации поля`email`
    
    - Отобразил под каждым полем формы ошибку в случае её наличия в массиве `errors` и стилизовал её. Ошибка хранится в свойстве название которого соответвует имени инпута, заданное ему при вызове функции `register()`.
    
    - Добавил валидатор для поля подтверждения пароля на минимальное количество символов

--- app/(sandbox)/(routes)/form-with-react-hook-form/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form/page.tsx
@@ -10,7 +10,7 @@ const FormWithReactHookForm = () => {
     reset,
     getValues,
     formState: { isSubmitting, isValid, errors },
-  } = useForm();
+  } = useForm({ mode: 'onBlur' });
 
   const onSubmit = async (data: FieldValues) => {
     const result = await new Promise((resolve, reject) =>
@@ -25,11 +25,18 @@ const FormWithReactHookForm = () => {
       <input
         {...register('email', {
           required: 'Email is required',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: 'Incorrect email',
+          },
         })}
         type='email'
         className='rounded px-4 py-2'
         placeholder='email'
       />
+      {errors?.email && (
+        <p className='text-red-500'>{`${errors.email.message}`}</p>
+      )}
       <input
         {...register('password', {
           required: 'Password is required',
@@ -42,14 +49,24 @@ const FormWithReactHookForm = () => {
         className='rounded px-4 py-2'
         placeholder='password'
       />
+      {errors?.password && (
+        <p className='text-red-500'>{`${errors.password.message}`}</p>
+      )}
       <input
         {...register('confirmPassword', {
           required: 'Confirm password is required',
+          minLength: {
+            value: 6,
+            message: 'Confirm password must be at least 6 characters',
+          },
         })}
         type='password'
         className='rounded px-4 py-2'
         placeholder='confirm password'
       />
+      {errors?.confirmPassword && (
+        <p className='text-red-500'>{`${errors.confirmPassword.message}`}</p>
+      )}
       <button
         type='submit'
         disabled={isSubmitting || !isValid}
