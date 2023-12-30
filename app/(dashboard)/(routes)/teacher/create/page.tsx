a3118a726f6eb46a815c0fb9288d5a5775cb475e
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-30 20:44:27 +0300

    Добавление компонента `Form`
    - Передал присвоенные хуком `useForm` значения в компонент `Form`, используя спред оператор.
    
    - Добавил нативный тег`form` и передал в его проп`onSubmit` функцию `handleSubmit`. Эта функция предотвращает стандартное поведение  функции-обработчика `onSubmit` и не вызывает её в случае наличия ошибок валидации.

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Form } from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -33,6 +34,11 @@ const CreatePage = () => {
           How would you like to name your course? Don&apos;t worry, you can
           change this later
         </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='mt-8 space-y-8'></form>
+        </Form>
       </div>
     </div>
   );
