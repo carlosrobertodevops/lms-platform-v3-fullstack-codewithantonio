341beba4dc643b109a56af0ed96ebc52ec77e478
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-30 11:33:22 +0300

    Добавил `formSchema`, используя `zod` для валидации полей формы
    - Импортировал `z` из библиотеки `zod`
    
    - Создал схему с помощью `zod`
    
    - Все свойства объекта конфигурации являются обязательными по умолчанию.
    Свойство `email` должно быть строкой и соответствовать формату электронной почты;
    Свойство `password` должно быть строкой и содеражать минимум 6 символов;
    Свойство `confirmPassword` должно быть строкой и соответствовало значению поля `password`;
    
    - У каждого валидатора есть кастомное сообщение об ошибке
    
    - Добавил метод `refine` после вызова метода `object`, чтобы получить доступ
    ко всем значениям полей и проверить совпадение паролей.
    
    - Указал под путь к инпуту, под которым будет выводиться ошибка при несовпадении паролей

--- app/(sandbox)/(routes)/form-with-react-hook-form-and-shadcn/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form-and-shadcn/page.tsx
@@ -1,5 +1,18 @@
 'use client';
 
+import { z } from 'zod';
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+    confirmPassword: z.string(),
+  })
+  .refine((allValues) => allValues.password === allValues.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  });
+
 const FormWithReactHookFormAndShadcn = () => {
   return <div>This is FormWithReactHookFormAndShadcn</div>;
 };
