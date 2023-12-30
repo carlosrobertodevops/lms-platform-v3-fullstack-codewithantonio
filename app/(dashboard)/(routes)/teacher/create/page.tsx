2f924ed215021c5c411d961158eb7ce151eea43d
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-30 13:19:46 +0300

    Добавил `FormSchemaType` для типизации значений полей ввода
    - Создал `FormSchemaType` c помощью метода `infer` (объекта `z`) и синтаксиса `typeof`

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -6,6 +6,8 @@ const formSchema = z.object({
   title: z.string().min(1, 'Title is required'),
 });
 
+type FormSchemaType = z.infer<typeof formSchema>;
+
 const CreatePage = () => {
   return <div>This is a create page</div>;
 };
