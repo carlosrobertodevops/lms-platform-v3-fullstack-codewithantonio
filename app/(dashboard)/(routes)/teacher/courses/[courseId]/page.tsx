ffeaaa36085d90480f7bd83e544a0704b2d2bec3
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-16 22:35:49 +0300

    Получил информацию о созданном курсе из базы данных
    - Для запроса к базе данных использовал синтаксис async/await
    
    - Использовал метод `findUnique` модели `course`, чтобы получить информацию о курсе, где уникальный идентификатор курса совпадает с параметром `courseId` из URL
    
    - Получил значение `userId` из функции `auth()`
    
    - Добавил переадрессацию на главную страницу, если курс не найден или `id` текущего пользователя не совпадает с `id` создателя курса

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,11 +1,22 @@
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
 interface CourseIdPageProps {
   params: {
     courseId: string;
   };
 }
 
-const CourseIdPage = ({ params }: CourseIdPageProps) => {
+const CourseIdPage = async ({ params }: CourseIdPageProps) => {
   const { courseId } = params;
+  const { userId } = auth();
+
+  const course = await db.course.findUnique({ where: { id: courseId } });
+
+  if (!course || userId !== course.userId) {
+    return redirect('/');
+  }
 
   return <div>{courseId}</div>;
 };
