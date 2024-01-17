506925c952c853f4eee30ed717ee94c57876c380
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-17 21:22:38 +0300

    Добавил опции для кастомной настройки компонента
    - Создал переменныe `backgroundVariants` и `iconVariants`
    
    - Присвоил им значение функции `cva`
    
    - Первым параметром функции `cva` передал общее дефолтное значение стилей компонента
    
    - Вторым параметром функции `cva` передал объект с кастомными настройками для модификации базовых стилей компонента

--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -1,5 +1,44 @@
+import { cva } from 'class-variance-authority';
+
 const IconBadge = () => {
-  return <div>This is an icon badge</div>;
+  const backgroundVariants = cva(
+    'flex items-center justify-center rounded-full',
+    {
+      variants: {
+        variant: {
+          default: 'bg-sky-100',
+          success: 'bg-emarald-100',
+        },
+        size: {
+          default: 'p-2',
+          sm: 'p-1',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+        size: 'default',
+      },
+    },
+  );
+
+  const iconVariants = cva('', {
+    variants: {
+      variant: {
+        default: 'text-sky-700',
+        success: 'text-emarald-700',
+      },
+      size: {
+        default: 'h-8 w-8',
+        sm: 'h-4 w-4',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  });
+
+  return <div className=''>This is an icon badge</div>;
 };
 
 export default IconBadge;
