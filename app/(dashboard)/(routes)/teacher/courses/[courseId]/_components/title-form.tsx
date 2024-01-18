0ced9876d302188358c95ccd7dcea407c153e69f
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-18 20:59:21 +0300

    Добавил схему и тип для валидации и типизации данных форм
    - Создал `titleFormSchema` для валидации данных формы, согласно заданным параметрам.
    
    - Создал тип `TitleFormSchemaType`, используя утилиту `infer` из библиотеки `zod` и синтаксиса `typeof`

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { z } from 'zod';
+
 interface TitleFormProps {
   initialData: { title: string };
   courseId: string;
 }
+
+const titleFormSchema = z.object({
+  title: z.string().trim().min(1, 'Title is required'),
+});
+
+type TitleFormSchemaType = z.infer<typeof titleFormSchema>;
 
 const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
   return <div>This is title form</div>;
