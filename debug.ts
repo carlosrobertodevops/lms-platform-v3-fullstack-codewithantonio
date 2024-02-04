1eb1a26300f3f1cf44f3e046045528cfdc46ea1c
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-04 21:38:31 +0300

    Удалил файл debug и добавил его в .gitignore

--- debug.ts
+++
@@ -1,5 +0,0 @@
-const requiredFields = ['Kotechka', 'Course about kotechka', null, null, null];
-
-const completedFields = requiredFields.filter((field) => Boolean(field));
-
-console.log(completedFields);
