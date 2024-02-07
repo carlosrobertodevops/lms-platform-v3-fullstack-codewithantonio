1b351a71ef4ae10acb30e2e54c74fcb0ddcdcff9
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-07 21:32:18 +0300

    Создал страницу для тестирования компонентов

---
+++ app/(sandbox)/(routes)/test/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { UploadDropZone } from '@/lib/uploadthing';
+
+const TestPage = () => {
+  return <div></div>;
+};
+
+export default TestPage;
--- app/(sandbox)/layout.tsx
+++ app/(sandbox)/layout.tsx
@@ -1,6 +1,6 @@
 const SandboxLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex h-full items-center justify-center bg-emerald-300'>
+    <div className='flex h-full items-center justify-center bg-slate-100'>
       {children}
     </div>
   );
