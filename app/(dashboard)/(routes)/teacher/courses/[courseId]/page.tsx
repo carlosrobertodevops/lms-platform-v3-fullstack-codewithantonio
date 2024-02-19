d5bdb1ad8d6294ab0e304e2816a50a236af776d1
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-19 20:49:37 +0300

    Создание компонента `AttachmentForm` с помощью клонирования `ImageForm`
    - В частной папке  `_components`, которые используются только для компонентов `CourseIdPage`, создан клон компонента `ImageForm`.
    
    - Изменено название файла и компонента на `attachment-form.tsx` и `AttachmentForm` соответственно.
    
    - Созданный компонент импортирован и добавлен в разметку `CourseIdPage` с теми же свойствами, что и `ImageForm`.

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -13,6 +13,7 @@ import DescriptionForm from './_components/description-form';
 import ImageForm from './_components/image-form';
 import CategoryForm from './_components/category-form';
 import PriceForm from './_components/price-form';
+import AttachmentForm from './_components/attachment-form';
 
 interface CourseIdPageProps {
   params: {
@@ -100,6 +101,7 @@ const CourseIdPage = async ({ params }: CourseIdPageProps) => {
               <IconBadge icon={File} />
               <h2 className='text-xl'>Resources & Attachments</h2>
             </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
           </div>
           {/* Section `Resources & Attachments` ends */}
         </div>
