4dd9b02f296849c9a5e0f87ba308935a46556f37
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-03-31 14:02:53 +0300

    Деструктуризация значений входящего `HTTP-запроса` и функции `auth`
    -  Деструктурировано значение `userId` из вызова функции `auth()`, полученной из модуля `@clerk/nextjs`.
    
    - Деструктурировано значение `url` из асинхронного вызова метода `json` у входящего `HTTP-запроса`.
    
    - Если `userId` не существует, то возвращается `new NextResponse` со  статусом  `401` и сообщением `Unauthorized`. Это необходимо для того, чтобы избежать ошибки тайпскрипта во время запроса к базе данных, т.к. `userId` может быть `null`.

--- app/api/courses/[courseId]/attachments/route.ts
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
 
 interface ContextProps {
   params: { courseId: 'string' };
@@ -6,6 +7,12 @@ interface ContextProps {
 
 export async function POST(request: NextRequest, { params }: ContextProps) {
   try {
+    const { userId } = auth();
+    const { url } = await request.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
   } catch (error) {
     console.log('[ATTACHMENTS]', error);
     return new NextResponse('Internal Error', { status: 500 });
