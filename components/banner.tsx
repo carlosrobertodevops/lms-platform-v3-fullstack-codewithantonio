3b7dc78f4152fa3c5d78d0d1e7ed2707fce17488
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-05 10:17:15 -0300

    Ajustes: Construindo o Banner

---
+++ components/banner.tsx
@@ -0,0 +1,43 @@
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { AlertTriangle, CheckCircleIcon } from 'lucide-react';
+
+const bannerVariants = cva(
+  'border text-center p-4 text-sm flex items-center w-full',
+  {
+    variants: {
+      variant: {
+        warning: 'bg-yellow-200/80 border-yelow-30 text-primary',
+        sucess: 'bg-emerald-700 border-emerald-800 text-secondary',
+      }
+    },
+    defaultVariants: {
+      variant: 'warning',
+    }
+  }
+)
+
+interface BannerProps extends VariantProps<typeof bannerVariants> {
+  label: string;
+};
+
+const iconMap = {
+  warning: AlertTriangle,
+  sucess: CheckCircleIcon,
+}
+
+export const Banner = ({
+  label,
+  variant,
+}: BannerProps) => {
+  const Icon = iconMap[variant || 'warning'];
+
+  return (
+    <div className={cn(bannerVariants({ variant }))}>
+      <Icon className="h-4 w-4 mr-2" />
+      { label }
+    </div>
+  )
+}
+
+export default Banner;
