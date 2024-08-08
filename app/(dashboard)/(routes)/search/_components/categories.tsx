2a5555a45a90195ba8414d846e12b8f4dc7fb27d
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-08 12:04:11 -0300

    Ajustes: Category Itens

--- app/(dashboard)/(routes)/search/_components/categories.tsx
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -1,10 +1,46 @@
 "use client"
 
-export const Categories = () => {
+import {Category} from "@prisma/client";
+import {
+  FcEngineering,
+  FcFilmReel,
+  FcMultipleDevices,
+  FcMusic,
+  FcOldTimeCamera,
+  FcSalesPerformance,
+  FcSportsMode
+} from "react-icons/fc";
+
+import { IconType } from "react-icons";
+import { CategoryItem } from "./category-item";
+
+interface CategoriesProps {
+  items: Category[];
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+  "Music" : FcMusic,
+  "Photography": FcOldTimeCamera,
+  "Fitness": FcSportsMode,
+  "Accounting": FcSalesPerformance,
+  "Computer Science": FcMultipleDevices,
+  "Engineering": FcEngineering,
+  "Filming": FcFilmReel,
+}
+
+export const Categories = ({
+  items,
+}: CategoriesProps) => {
   return (
-    <div>
-      Categories!
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      { items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
     </div>
   )
-
 }
