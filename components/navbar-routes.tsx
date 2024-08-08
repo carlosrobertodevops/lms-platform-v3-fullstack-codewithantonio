51c98d29afd943a9c876750e9c0b047a75f87046
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-08 16:50:10 -0300

    Ajustes: Search

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,38 +1,50 @@
 'use client';
 
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { UserButton } from '@clerk/nextjs';
 import { LogOut } from 'lucide-react';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { SearchInput } from './search-input';
+
+// import { React } from "react";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith('/teacher');
   const isPlayerPage = pathname?.includes('/chapters');
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className='ml-auto flex items-center gap-x-2'>
-      {isTeacherPage || isPlayerPage ? (
-        <Link href={'/'}>
-          <Button size={'sm'} variant={'ghost'}>
-            <LogOut size={16} className='mr-2' />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href={'/teacher/courses'}>
-          <Button size={'sm'} variant={'ghost'}>
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block" >
+          <SearchInput />
+        </div>
       )}
-      <UserButton
-        afterSignOutUrl='/'
-      />
-    </div>
-  );
-};
+
+      <div className='ml-auto flex items-center gap-x-2'>
+        {isTeacherPage || isPlayerPage ? (
+          <Link href={'/'}>
+            <Button size={'sm'} variant={'ghost'}>
+              <LogOut size={16} className='mr-2' />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href={'/teacher/courses'}>
+            <Button size={'sm'} variant={'ghost'}>
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton
+          afterSignOutUrl='/'
+        />
+      </div>
+    </>
+  )
+}
 
 export default NavbarRoutes;
---
+++ components/search-input.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+export const SearchInput = () => {
+
+
+  return (
+    <div className="relative">
+      <Search
+        className="w-4 h-4 text-slate-600 absolute top-3 left-3"
+      />
+      <Input
+        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-salte-200"
+        placeholder="Search for a course"
+      />
+    </div>
+  )
+}
