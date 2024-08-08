fa71f60c64900aa7700172e39d46a3817720594b
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-08 18:27:52 -0300

    Ajustes: search-input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,13 +1,34 @@
 "use client"
 
+import { useEffect, useState } from "react";
+
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import {useDebounce} from "@/hooks/use-debounce"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 export const SearchInput = () => {
 
-  const [value, setValue] = useState("")
-  const debouncedValue =  useDebugValue(value)
+  const [value, setValue] = useState("");
+  const debouncedValue =  useDebounce(value);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const currentCategoryId = searchParams.get("categoryId");
+
+
+  useEffect(() => {
+    const url = qs.stringifyUrl({
+      url: pathname,
+      query:{
+        categoryId: currentCategoryId,
+        title: debouncedValue,
+      }
+    }, { skipEmptyString: true, skipNull: true });
+  }, [debouncedValue, currentCategoryId, router, pathname])
 
 
   return (
