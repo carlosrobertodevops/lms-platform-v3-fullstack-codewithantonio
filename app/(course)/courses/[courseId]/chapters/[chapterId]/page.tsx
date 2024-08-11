24d151e8e121adda8ea154a5c85099e257be8c90
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-11 18:51:52 -0300

    Ajustes: ChapterIdPage

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,16 @@
-const ChapterIdPage = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params
+}: {
+  params: { couseId: string; chapterId: string }
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
   return (
     <div>
       Chapter Id!
