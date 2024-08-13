531ff2afe7e4a7750a2a18e75ecc03dd82fbef94
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-13 19:44:34 -0300

    Ajustes: middleware

--- middleware.ts
+++ middleware.ts
@@ -12,12 +12,14 @@
 
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+//   publicRoutes: ["/api/webhook"]
+})
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!.*\\..*|_next).*)", "/", "/app/api/", "/app/",
+    "/((?!.*\\..*|_next).*)", "/", "/app/api", "/app", "/api/webhook",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
