21d70c73f149d176196e7dd61b5eac563ea0b79e
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-15 20:19:24 -0300

    Ajustes: Clerk

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <SignUp
+      appearance={{
+        elements: {
+          formButtonPrimary: {
+            fontSize: 12,
+          },
+        },
+      }}
+    />
+  );
 }
