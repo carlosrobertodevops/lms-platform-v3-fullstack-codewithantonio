dfcbae401190403e4c5a4732103d863faa36f3c8
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-17 20:02:26 -0300

    Ajustes: Video Player

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -29,6 +29,8 @@ export const VideoPlayer = ({
   title,
 }: VideoPlayerProps) => {
   const [isReady, setIsReady] = useState(false);
+  const router = useRouter();
+  const confetti = useConfettiStore();
 
   return (
     <div className="relative aspect-video">
