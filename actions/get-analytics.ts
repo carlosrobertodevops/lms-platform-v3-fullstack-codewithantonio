e8795cebc7013ee7d48c04993a2b7abbb59dd054
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-18 12:23:32 -0300

    Ajustes !!!

---
+++ actions/get-analytics.ts
@@ -0,0 +1,35 @@
+import { db } from "@/lib/db";
+import { Course, Purchase } from "@prisma/client";
+
+type PurchaseWithCourse = Purchase & {
+  course: Course;
+};
+
+const groupByCourse = (purchases: PurchaseWithCourse[]) => {
+  const grouped: { [courseTitle: string]: number } = {};
+
+  purchases.forEach((purchase) => {
+    const courseTitle = purchase.course.title;
+
+    if (!grouped[courseTitle]) {
+      grouped[courseTitle] = 0;
+    }
+    grouped[courseTitle] += purchase.course.price!;
+  });
+
+  return grouped;
+};
+
+
+export const getAnalytics = async (userId: string) => {
+  try {
+
+  } catch (error) {
+    console.log(" [GET_ANALYTICS]", error);
+    return {
+      data: [],
+      totalRevenue: 0,
+      totalSales: 0,
+    }
+  }
+}
