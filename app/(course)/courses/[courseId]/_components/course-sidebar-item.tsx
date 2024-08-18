c968efd4fc0e582818c910f683369072106363ac
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-08-17 21:59:16 -0300

    Ajustes: Proccess

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -37,7 +37,7 @@ export const CourseSidebarItem = ({
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 translate-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive && "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
         isCompleted && "text-emerald-700 hover:text-emerald-700",
-        isCompleted && isActive && "text-emerald-700",
+        isCompleted && isActive && "text-emerald-700 bg-emerald-300/30",
       )}
     >
       <div className="flex items-center gap-x-2 py-4">
